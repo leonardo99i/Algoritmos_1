f3fca56ed089f2a3b87912f0fae5e2493e64c5c8
Leonardo Ribeiro Leonardi <49618344+leonardo99i@users.noreply.github.com>
2021-08-26 16:22:48 -0300

    Codificando o Exs_1 Para saber o valor mais barato

--- Produtos/src/produtos/TestaMenorPreco.java
+++ Produtos/src/produtos/TestaMenorPreco.java
@@ -2,7 +2,23 @@ package produtos;
 
 public class TestaMenorPreco {
     public static void main(String[] args) {
-        
+        double precos[] = new double[5];
+        precos [0] = 1000000;
+        precos[1] = 46000;
+        precos[2] = 16000;
+        precos[3] = 46000;
+        precos[4] = 17000;
+
+        int maisBarato = 0;
+        int atual = 0;
+
+        for(atual = 0; atual <= 4; atual++){
+            if(precos[atual] < precos[maisBarato]){
+                maisBarato = atual;
+            }
+        }
+        System.out.println("O mais barato é: " + maisBarato);
+        System.out.println("O carro mais barato custa R$ " + precos[maisBarato]);
     }
     
 }
